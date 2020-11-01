7b7d5b6214d35740c36544d83a9275c6347dc8e6
czh501429 <2283151058@qq.com>
2020-11-01 11:06:32 +0800

    11.1a

--- pages/home/home.js
+++ pages/home/home.js
@@ -32,8 +32,6 @@ Page({
             },
         ],
         color:'yellow',
-        backIcon:'true',
-        backHome:'true',
         inputValue:'',
         swiperList: [
             {
@@ -51,27 +49,41 @@ Page({
         itemId: 0,
         switchDuration: 500,
         autoPlayInterval: 2000,
-        slider: [{
-            imageUrl: 'https://b.bdstatic.com/searchbox/icms/searchbox/img/swiperA.png'
-        }, {
-            imageUrl: 'https://b.bdstatic.com/searchbox/icms/searchbox/img/swiperB.png'
-        }, {
-            imageUrl: 'https://b.bdstatic.com/searchbox/icms/searchbox/img/swiperC.png'
-        }],
+        // slider: [{
+        //     imageUrl: 'https://b.bdstatic.com/searchbox/icms/searchbox/img/swiperA.png'
+        // }, {
+        //     imageUrl: 'https://b.bdstatic.com/searchbox/icms/searchbox/img/swiperB.png'
+        // }, {
+        //     imageUrl: 'https://b.bdstatic.com/searchbox/icms/searchbox/img/swiperC.png'
+        // }],
         swiperCurrent: 0,
         currentTab: 0,
-        locationInfo:'定位',
+        locationInfo:'',
         infoList: [
-        // {
-        //     chineseName: '国家',
-        //     engName: 'country',
-        //     value: ''
-        // },
         {
             chineseName: '城市名称',
             engName: 'city',
             value: ''
         }]
+    },
+    onShow(e){
+        var locationInfo=''
+        swan.getLocation({
+            type: 'gcj02',
+            success: res => {
+                let infoList = this.getData('infoList');
+                locationInfo=res['city']
+                this.setData({
+                    locationInfo:locationInfo,
+                });
+            },
+            fail: err => {
+                swan.showToast({
+                    title: '获取失败,请检查位置授权是否开启',
+                    icon: 'none'
+                });
+            }
+        });
     },
     swiperChange(e) {
         console.log('swiperChange:', e.detail);
@@ -85,32 +97,6 @@ Page({
 
         });
         console.log(this.getData('inputValue'))
-    },
-
-    getLocation(e) {
-        var locationInfo='';
-        swan.getLocation({
-            type: 'gcj02',
-            success: res => {
-                let infoList = this.getData('infoList');
-                for (var i = 0; i < infoList.length; i++) {
-                    const engName = infoList[i].engName;
-                    locationInfo=locationInfo+res[engName];
-                    infoList[i].value = res[engName];
-                }
-                console.log(locationInfo);
-                this.setData({
-                    locationInfo:locationInfo,
-                    infoList:infoList,
-                });
-            },
-            fail: err => {
-                swan.showToast({
-                    title: '获取失败,请检查位置授权是否开启',
-                    icon: 'none'
-                });
-            }
-        });
     },
     toLeft() {
         swan.showToast({
